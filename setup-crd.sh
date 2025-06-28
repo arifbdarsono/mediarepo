a807fba78609ff76eee9d563a0e8fc8f427fa13a
arifbdarsono <71606151+arifbdarsono@users.noreply.github.com>
2025-06-28 20:18:16 +0700

    Update setup-crd.sh

--- setup-crd.sh
+++ setup-crd.sh
@@ -1,1 +1,1 @@
-DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AVMBsJhA3pZCej_l23W6ezYa_GWNsVuSAXXgnTiFxQv76xPhW2I19z5EtH8r-L_3sCH4iw" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
+DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AVMBsJhA3pZCejXXX" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
